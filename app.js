fb9b36deaa616ccf1a1a845b48bc3bca739efcde
Allie Liu <allieliu@outlook.com>
2016-03-21 17:21:15 -0700

    commit

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
 console.log('it works i guess');
 
+var player1 = {
+  name: 'Allie',
+  icon: 'img',
+  position: 0,
+  last_roll: null
+};
+
+function randomNumber() {
+  return Math.floor(Math.random() * 6) + 1;
+}
 
 function rollDice(){
-  var die1 = document.getElementById('die1');
-  var die2 = document.getElementById('die2');
+  var die1 = document.getElementById('die1'); // jquery selecting
+  var die2 = document.getElementById('die2'); // regular dom
   var status = document.getElementById('status');
-  var d1 = Math.floor(Math.random() * 6) + 1;
-  var d2 = Math.floor(Math.random() * 6) + 1;
-  var diceTotal = d1 + d2;
+  var d1 = randomNumber();
+  var d2 = randomNumber();
+  // making diceTotal global so we can reference it in other functions
+  diceTotal = d1 + d2;
   die1.innerHTML = d1;
   die2.innerHTML = d2;
   status.innerHTML = "You rolled "+diceTotal+".";
   if(d1 === d2 ){
     status.innerHTML += " DOUBLES! You get a FREE TURN!";
   }
+  player1.position = diceTotal;
 }
+
+// player placement
+// icon
+// player name
+// player last roll
+
+
 
 $(document).ready(function(){
     $("marker1").click(function(){
